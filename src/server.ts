40c11731cd4770798cc9ca6c8ed1220fbb5e545b
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2024-05-01 11:17:11 -0300

    added building to js

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express, { Express, Request, Response } from "express";
+
+const app = express();
+const port = process.env.PORT || "8080";
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("Welcome home!")
+});
+
+app.listen(port, () => {
+    console.log(`[server]: server running at 0.0.0.0:${port}`)
+})
