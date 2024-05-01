6b6d541929c97eb86896da72946cc6d330b92eec
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2024-05-01 13:01:29 -0300

    added eslint and prettier

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import express, { Express, Request, Response } from "express";
+import express, { Request, Response } from 'express';
 
 const app = express();
-const port = process.env.PORT || "8080";
+const port = process.env.PORT || '8080';
 
 app.get('/', (req: Request, res: Response) => {
-    res.send("Welcome home!")
+  res.send('Welcome home!');
 });
 
 app.listen(port, () => {
-    console.log(`[server]: server running at 0.0.0.0:${port}`)
-})
+  console.log(`[server]: server running at 0.0.0.0:${port}`);
+});
